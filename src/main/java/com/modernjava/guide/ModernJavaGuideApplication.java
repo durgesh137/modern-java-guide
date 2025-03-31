965667cf4ae93ccca2ad2ea9b935cd1068632abf
durgeshkumar <kdurgesh137@gmail.com>
2025-03-31 21:08:00 +0530

    feat: initial commit with base structure

---
+++ src/main/java/com/modernjava/guide/ModernJavaGuideApplication.java
@@ -0,0 +1,13 @@
+package com.modernjava.guide;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModernJavaGuideApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ModernJavaGuideApplication.class, args);
+	}
+
+}
