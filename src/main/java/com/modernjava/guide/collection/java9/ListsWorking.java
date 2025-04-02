ea68d470bc1f7d33f30613861f32cb63fa9e530e
durgeshkumar <kdurgesh137@gmail.com>
2025-04-03 00:23:02 +0530

    list working within java8 and java9

--- src/main/java/com/modernjava/guide/collection/java9/ListsWorking.java
+++ src/main/java/com/modernjava/guide/collection/java9/ListsWorking.java
@@ -1,6 +1,5 @@
-package com.modernjava.guide.collection;
+package com.modernjava.guide.collection.java9;
 
-import java.util.Arrays;
 import java.util.List;
 
 public class ListsWorking {
@@ -9,9 +8,9 @@ public class ListsWorking {
     }
 
     private static void workingWithArraysListMethod() {
-        List<Integer> nums = Arrays.asList(101, 901,5001, 400, 1, 9, 1000);
+        List<Integer> nums = List.of(101, 901,5001, 400, 1, 9, 1000);
 
-        // addition not allowed, since nums is fixed-size list backed by original array
+        // this is immutable list, ImmutableCollection
         nums.add(null);//UnsupportedOperationException thrown
         System.out.println("List: "+nums);
 
